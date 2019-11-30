5089cd1e91268f1ff1a2e130f42ddd64102fc422
Noah Scala <tobi8912@gmail.com>
2019-11-30 03:24:09 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # Your Code Here
+def map(source_array)
+  new_array = []
+  index = 0 
+  while index < source_array.length do 
+    new_array.push(yield(source_array[index]))
+    index += 1
+  end
+  new_array
+end 
+
+def reduce(source_array, start_value = nil)
+  if start_value
+    running_total = start_value
+    index = 0 
+  else 
+    running_total = source_array[0]
+    index = 1 
+  end
+  while index < source_array.length do
+    running_total = yield(running_total, source_array[index]) 
+    index += 1 
+  end 
+  running_total
+end 
