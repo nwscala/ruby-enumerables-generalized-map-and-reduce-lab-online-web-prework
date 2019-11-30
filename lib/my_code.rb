56622f68712a594b449e510eecfa8a9e34e9c86f
Noah Scala <tobi8912@gmail.com>
2019-11-30 03:13:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,7 +3,7 @@ def map(source_array)
   new_array = []
   index = 0 
   while index < source_array.length do 
-    yield(source_array[index])
+    new_array.push(yield(source_array[index]))
     index += 1
   end 
 end 
