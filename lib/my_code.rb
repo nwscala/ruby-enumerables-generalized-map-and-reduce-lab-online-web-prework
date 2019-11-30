2af5df05ecedab53278fcf6cec451d6af52a34ed
Noah Scala <tobi8912@gmail.com>
2019-11-30 03:20:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,7 +9,7 @@ def map(source_array)
   new_array
 end 
 
-def reduce(source_array, start_value = 0)
+def reduce(source_array, start_value = nil)
   running_total = 0 
   index = 0 
   while index < source_array.length do 
